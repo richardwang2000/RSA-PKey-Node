e8b31a47fb8f48cb392933538d93bc96aa239eee
Richard Wang <richard.wang@york.ca>
2019-03-31 17:16:28 -0400

    Update Index.js

--- index.js
+++ index.js
@@ -62,3 +62,13 @@ function encodeLengthHex(n) {
         return toHex(length_of_length_byte)+n_hex
     }
 }
+
+
+var modulus =  "tVKUtcx_n9rt5afY_2WFNvU6PlFMggCatsZ3l4RjKxH0jgdLq6CScb0P3ZGXYbPzXvmmLiWZizpb-h0qup5jznOvOr-Dhw9908584BSgC83YacjWNqEK3urxhyE2jWjwRm2N95WGgb5mzE5XmZIvkvyXnn7X8dvgFPF5QwIngGsDG8LyHuJWlaDhr_EPLMW4wHvH0zZCuRMARIJmmqiMy3VD4ftq4nS5s8vJL0pVSrkuNojtokp84AtkADCDU_BUhrc2sIgfnvZ03koCQRoZmWiHu86SuJZYkDFstVTVSR0hiXudFlfQ2rOhPlpObmku68lXw-7V-P7jwrQRFfQVXw"
+//exponent should be base64/base64url
+var exponent = "AQAB";
+
+var pem = rsaPublicKeyPem(modulus, exponent);
+
+console.log(pem);
+
